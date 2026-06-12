80c71cfedf31abd52255ba130a8b835dfd998a57
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-06-12 23:56:13 +0300

    Add a section to Upload that allows users to upload a PDF CV

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -3,9 +3,15 @@ import CvSvg from './svgs/CvSvg';
 export default function Upload() {
   return (
     <section className="col-start-1 col-end-2 row-span-8 rounded-lg p-5 bg-[#30302E] text-[#9c9a92]">
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 py-6">
         <CvSvg />
         <p>Your Resume</p>
+      </div>
+
+      <div className="bg-[#262624] flex flex-col justify-center items-center h-[50%] gap-1 rounded-lg">
+        <CvSvg />
+        <p className="pt-1 text-lg">Drop your PDF here</p>
+        <p>or click to browse</p>
       </div>
     </section>
   );
