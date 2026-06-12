a17516ffa77fd5205c3aeeb5bd14f01c0deb7cdf
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-06-12 23:42:37 +0300

    Create and import Cv svg

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,7 +1,12 @@
+import CvSvg from './svgs/CvSvg';
+
 export default function Upload() {
   return (
     <section className="col-start-1 col-end-2 row-span-8 rounded-lg p-5 bg-[#30302E] text-[#9c9a92]">
-      Your Resume
+      <div className="flex items-center gap-1">
+        <CvSvg />
+        <p>Your Resume</p>
+      </div>
     </section>
   );
 }
