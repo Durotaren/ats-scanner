39d749efb062860bcf8fbd9bb06ee068e1a416d8
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-06-14 23:40:04 +0300

    Add a dotted background to pdf drop

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -3,12 +3,12 @@ import CvSvg from './svgs/CvSvg';
 export default function Upload() {
   return (
     <section className="col-start-1 col-end-2 row-span-8 rounded-lg p-5 bg-[#30302E] text-[#9c9a92]">
-      <div className="flex items-center gap-1 py-6">
+      <div className="flex items-center gap-1 pb-6">
         <CvSvg />
         <p>Your Resume</p>
       </div>
 
-      <div className="bg-[#262624] flex flex-col justify-center items-center h-[50%] gap-1 rounded-lg">
+      <div className="bg-[#262624] flex flex-col justify-center items-center h-[50%] gap-1 rounded-lg border-dashed border-2 border-[#9c9a92] cursor-pointer">
         <CvSvg />
         <p className="pt-1 text-lg">Drop your PDF here</p>
         <p>or click to browse</p>
