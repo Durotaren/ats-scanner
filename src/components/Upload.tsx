47967d5ae0d1c2058cafb4164b68ef4358504a77
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-06-14 23:48:29 +0300

    Create an element between pdf drop and text paste

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -8,10 +8,16 @@ export default function Upload() {
         <p>Your Resume</p>
       </div>
 
-      <div className="bg-[#262624] flex flex-col justify-center items-center h-[50%] gap-1 rounded-lg border-dashed border-2 border-[#9c9a92] cursor-pointer">
+      <div className="bg-[#262624] flex flex-col justify-center items-center h-[45%] gap-1 rounded-lg border-dashed border-2 border-[#9c9a92] cursor-pointer">
         <CvSvg />
         <p className="pt-1 text-lg">Drop your PDF here</p>
         <p>or click to browse</p>
+      </div>
+
+      <div className="flex mt-3 items-center gap-2">
+        <div className="w-[43%] bg-[#9c9a92] h-[1px]"></div>
+        <p className="text-lg">or paste text</p>
+        <div className="w-[42%] bg-[#9c9a92] h-[1px]"></div>
       </div>
     </section>
   );
